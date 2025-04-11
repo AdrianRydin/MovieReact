fc8e70faa4f4e6a6003082ad8a5d7b84bfe44297
AdrianRydin <adrian.rydin@gmail.com>
2025-04-11 12:22:29 +0200

    Fixed searchResult component

--- src/Pages/SearchResult/SearchResult.jsx
+++ src/Pages/SearchResult/SearchResult.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./searchResult.css";
+import { useLocation } from "react-router-dom";
 
-function SearchResult() {
-  return <div></div>;
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function SearchResult({ movies, setMovies }) {
+  const queryParam = useQuery().get("query");
+
+  useEffect(() => {
+    if (!queryParam) return;
+    fetch(`http://www.omdbapi.com/?apikey=6d4dd304&s=${queryParam}*`)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.Search || []))
+      .catch((error) => console.error("API Error: ", error));
+  }, [queryParam, setMovies]);
+
+  return (
+    <div className="search-result-container">
+      {movies && movies.length > 0
+        ? movies.map((movie) => (
+            <div key={movie.imdbID} className="movie-result">
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+              <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+            </div>
+          ))
+        : queryParam && <p>No results found.</p>}
+    </div>
+  );
 }
 
 export default SearchResult;
