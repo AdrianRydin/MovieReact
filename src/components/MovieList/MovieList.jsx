25ac468d6a7ceb556d2c48dfa977ac52d921e903
HasanAlshehabi <Hasan.alshehabi99@gmail.com>
2025-04-14 10:48:28 +0200

    added shuffle så att trailers slumpas

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,8 @@
+import React from "react";
 import "./movieList.css";
-import React, { useEffect, useState } from 'react';
-import MovieCard from "../MovieCard/MovieCard";
 
-const MovieList = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const fetchTopMovies = async () => {
-      try {
-        const response = await fetch('https://santosnr6.github.io/Data/favoritemovies.json');
-        if (!response.ok) throw new Error('Failed to fetch top movies');
-        const data = await response.json();
-        const shuffled = [...data].sort(() => Math.random() - 0.5);
-        setMovies(shuffled);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchTopMovies();
-  }, []);
-
-  return (
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <MovieCard key={movie.imdbID} movie={movie} />
-      ))}
-    </div>
-  );
-};
+function MovieList() {
+  return <div></div>;
+}
 
 export default MovieList;
